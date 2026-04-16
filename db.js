660f62ee6a11fdf569fe6b5fbcb93cac177bf1e9
DIEGO DE NEGREIROS SOARES <diegodenegreirossoares@gmail.com>
2026-04-16 09:22:45 -0300

    update 1

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const { Client } = require('pg');
 require('dotenv').config();
 
 const client = new Client({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: 'localhost',
+  port: 3001,
+  user: 'postgres',
+  password: 3214,
+  database: 'postgres',
 });
 
 module.exports = client;
